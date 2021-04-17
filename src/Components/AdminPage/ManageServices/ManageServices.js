c87313d858ccb189e7371a342bc938aa859a4a78
hasan178821 <hasanrana178821@gmail.com>
2021-04-17 16:54:01 +0600

    change manage services data

--- src/Components/AdminPage/ManageServices/ManageServices.js
+++ src/Components/AdminPage/ManageServices/ManageServices.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ManageServicesDetails from '../ManageServicesDetails/ManageServicesDetails';
 import './ManageServices.css';
 
-const ManageServices = ({allOrderedList}) => {
+const ManageServices = () => {
+    const [manageServiceList, setManageServiceList] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8050/getServiceData')
+        .then(res => res.json())
+        .then(data => setManageServiceList(data));
+    }, [])
     return (
         <div className="manage-services">
             <div className="row manage-services-title">
@@ -11,7 +18,7 @@ const ManageServices = ({allOrderedList}) => {
                 <div className="col-md-3"><h5>Action</h5></div>
             </div>
             {
-                allOrderedList.map(manageService => <ManageServicesDetails manageService={manageService} key={manageService._id}></ManageServicesDetails>)
+                manageServiceList.map(manageService => <ManageServicesDetails manageService={manageService} key={manageService._id}></ManageServicesDetails>)
             }
         </div>
     );
--- src/Components/AdminPage/ManageServicesDetails/ManageServicesDetails.js
+++ src/Components/AdminPage/ManageServicesDetails/ManageServicesDetails.js
@@ -17,8 +17,8 @@ const ManageServicesDetails = ({manageService}) => {
     }
     return (
         <div className="row ps-3 manage-service-list">
-            <div className="col-md-6"> <h5>{manageService.serviceName}</h5> </div>
-            <div className="col-md-3"> <h5>{manageService.servicePrice}</h5> </div>
+            <div className="col-md-6"> <h5>{manageService.name}</h5> </div>
+            <div className="col-md-3"> <h5>{manageService.price}</h5> </div>
             <div className="col-md-3 ps-1 pt-2 action-icon">
                 <span style={{color: '#0d6efd'}}> <FontAwesomeIcon icon={faEdit}/> </span>
                 <span onClick={() => handleDeleteService(manageService._id)} style={{color: 'red'}}> <FontAwesomeIcon icon={faTrashAlt}/> </span>
